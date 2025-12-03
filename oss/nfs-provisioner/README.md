b72cb9b3c75caa195e8d3640736196443e321181
tryoo0607 <tryoo0607@gmail.com>
2025-12-03 12:17:58 +0900

    oss/nfs-provisioner : 구성 추가

---
+++ oss/nfs-provisioner/README.md
@@ -0,0 +1,6 @@
+# nfs-provisioner
+- Kubernetes에서 NFS 스토리지를 StorageClass로 사용하기 위해 배포
+- PVC 요청 시 NFS에 자동으로 PV를 생성·관리해주는 역할
+
+<br/>
+<br/>
---
+++ oss/nfs-provisioner/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+
+helm install nfs-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+  --namespace nfs-provisioner \
+  --set nfs.server=192.168.0.24 \
+  --set nfs.path=/data \
+  --set nfs.mountOptions="{vers=3,rsize=1048576,wsize=1048576,noatime,nodiratime,async,hard}" \
+  --set storageClass.name=nfs-client \
+  --set storageClass.defaultClass=true
